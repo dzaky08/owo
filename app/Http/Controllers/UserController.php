22019943383ff2f121e29790958718dc1111f180
SiFadly00 <fadlyakbar444@gmail.com>
2024-01-22 21:10:13 +0700

    edit by fadly

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DetailTransaksi;
 use Illuminate\Http\Request;
 use App\Models\Hotel;
+use App\Models\Transaksi;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -17,5 +20,35 @@ class UserController extends Controller
     {
         $hotel = Hotel::all();
         return view('beranda', compact('hotel'));
+    }
+
+    public function bayar(detailtransaksi $detailtransaksi)
+    {
+        $kamar = $detailtransaksi->kamar;
+        return view('bayar', compact('detailtransaksi','kamar'));
+    }
+
+    public function postbayar(Request $request, DetailTransaksi $detailtransaksi)
+    {
+        $request->validate([
+            'bukti_transaksi'=>'required',
+        ]);
+        $transaksi = Transaksi::create([
+            'user_id' => auth()->id(),
+            'totalharga' => $detailtransaksi->totalharga,
+            'kode'=>'INV'.Str::random(8)
+        ]);
+        $detailtransaksi->update([
+            'transaksi_id'=>$transaksi->id,
+            'status'=>'cekout',
+            'bukti_transaksi'=>$request->bukti_transaksi->store('img/buktipembayaran'),
+        ]);
+        return redirect()->route('sumary')->with('status', 'Berhasil Upload bukti');
+    }
+
+    public function sumary(Request $request)
+    {
+        $detailtransaksi = detailtransaksi::where('user_id', auth()->id())->where('status','cekout')->get();
+        return view('sumary', compact('detailtransaksi'));
     }
 }
