98f8e1268c79df19926516470129ab4c3d81f668
SiFadly00 <fadlyakbar444@gmail.com>
2024-01-23 11:58:40 +0700

    menambahkan background pada view login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,13 @@
 @extends('template.html')
 @section('title','home')
+<style>
+    body {
+        background-image: url('https://ik.imagekit.io/tvlk/blog/2022/06/Hotel-di-Jakarta-dengan-Balkon-Somerset-Sudirman-Jakarta-2-1024x683.jpeg?tr=dpr-2,w-675');
+        background-repeat: no-repeat;
+        background-size: 100%
+        
+    }
+</style>
 @section('body')
     <div class="container mt-5">
         <div class="card mx-auto p-5 w-25">
